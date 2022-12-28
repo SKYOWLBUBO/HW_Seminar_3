9811c8f1c5501b57c03344f434e854ab9c455b62
Matvey Firsov <firsov.mat@yandex.ru>
2022-12-28 14:19:51 +0300

    HW

---
+++ Task27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Prompt(string message)
+{
+    System.Console.WriteLine("введите число");
+    return int.Parse(Console.ReadLine());
+}
+int n = Prompt("Введите число");
+int summ = 0;
+
+while (n > 0)
+{
+    summ = summ + n % 10;
+    n = n / 10;
+}
+System.Console.WriteLine(summ);
+
+
+
+
---
+++ Task29/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33] 
+
+int[] array(int Length = 8)
+{
+    int[] tempArray = new int[Length];
+    for(int i = 0; i < Length; i++)
+    {
+        tempArray[i] = new Random(). Next();
+    }
+    return tempArray;
+}
+void PrintArray(int[] intArray)
+{
+     for(int i = 0; i < intArray.Length; i++)
+    {
+        System.Console.WriteLine(value: $"{intArray[i]}");
+    }
+    
+}
+
+
+System.Console.WriteLine("случайный массив длинной 8 ");
+PrintArray(intArray: array());
